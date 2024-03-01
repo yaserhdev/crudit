bee3029eaca092d83d771824f601926362e8e9f4
pebbbles <alondra@Kiaras-MBP.routerlogin.net>
2024-02-29 21:15:10 -0500

    add user.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection.js');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -14,15 +18,31 @@ User.init(
     },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
         type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
     },
     password: {
         type: DataTypes.STRING,
+        allowNull: false,
+      validate: {
+        len: [8],
+      },
     }
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
