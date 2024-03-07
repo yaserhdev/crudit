223f783d042993dc1886ca58656b0ea0c96ae6e6
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-03-06 20:46:09 -0500

    Added package.json file with all dependencies, converted postData.js to a .json file, edited seeds.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/connection.js');
 const { User, Post } = require('../models');
 const userData = require('./userData.json');
-const postData = require('./postData.js')
+const postData = require('./postData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
