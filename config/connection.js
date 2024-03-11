a7fea77c6b0a2d7e4888f63fc9321c64dc1c78b6
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-03-11 18:01:57 -0400

    Heroku updates

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,23 @@ const Sequelize = require('sequelize');
 // Require dotenv
 require('dotenv').config();
 // Create connection to database
-const sequelize = new Sequelize(
-    // Database name from .env file
-    process.env.DB_NAME,
-    // User name from .env file
-    process.env.DB_USER,
-    // Password from .env file
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-    }
-);
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        // Database name from .env file
+        process.env.DB_NAME,
+        // User name from .env file
+        process.env.DB_USER,
+        // Password from .env file
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
 // Export module
 module.exports = sequelize;
