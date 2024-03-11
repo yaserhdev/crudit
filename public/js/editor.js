c520cf5f68b22a9bec60fa735f577cd438bc7063
Tyler Clark <tylerc111@gmail.com>
2024-03-11 13:46:34 -0400

    added some CSS functionality, added some code to editor.js

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,4 +1,27 @@
+// Initialize the Quill editor
 var editor = new Quill('#editor', {
-    modules: { toolbar: '#toolbar' },
-    theme: 'snow'
-  });
+  modules: {
+    toolbar: '#toolbar'
+  },
+  theme: 'snow'
+});
+
+// Get the hidden textarea in the form
+var formContent = document.querySelector('#content');
+
+// Update the Quill editor content when the form content changes
+editor.on('text-change', function() {
+  formContent.value = editor.root.innerHTML;
+});
+
+// Update the Quill editor content when the form is submitted
+document.querySelector('#new-post-form').addEventListener('submit', function(event) {
+  // Prevent the default form submission
+  event.preventDefault();
+  
+  // Update the hidden textarea value with the Quill editor content
+  formContent.value = editor.root.innerHTML;
+  
+  // Submit the form normally
+  this.submit();
+});
